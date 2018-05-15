c19350e0cc791f8c633fa6c92abff5bd39bd131d
emil38a7 <emilk@DESKTOP-I0SDNNG>
2018-05-15 13:37:21 +0200

    Models Added

--- ArchiveGameServer/app.js
+++ ArchiveGameServer/app.js
@@ -1,3 +1,14 @@
-'use strict';
+var express = require('express');
+var app = express();
 
-console.log('Hello world');
+
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
